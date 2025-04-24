12039c4148cb13a95e5f15f5d0959238e54089d8
mpflynnx <mpflynnx@gmail.com>
2025-04-24 14:50:20 +0000

    Add problem2.5.md and associated code

---
+++ section2/practice-problems/code/problem2dot5/main.c
@@ -0,0 +1,60 @@
+/*
+*  This program is a simple example of how to use the show_bytes function to print the bytes of a variable.
+* The show_bytes function is a simple function that takes a pointer to a variable and the size of the variable
+* and prints the bytes of the variable in hexadecimal format.
+* The show_int, show_float, and show_pointer functions are just wrappers for the show_bytes function.
+* The test_show_bytes function is a simple test function that demonstrates how to use the show_bytes function.
+* The test_show_bytes function takes an integer value as input and prints the bytes of the integer value, the float value
+* of the integer value, and the pointer value of the integer value.
+* The main function is just a simple main function that calls the test_show_bytes function with an integer value.
+* The output of the program is the bytes of the integer value, the float value of the integer value, and the pointer value of the integer value.
+* The output of the program is in hexadecimal format.
+
+* https://github.com/quchunguang/test/blob/dd1dde14a69d9e8b2c9ed3efbf536df7840f0487/bookcsapp/code/data/show-bytes.c
+*
+*/
+
+#include <stdio.h> // for printf
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len){
+    size_t i;
+    for(i = 0; i < len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x){
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x){
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x){
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val){
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
+
+
+int main(){
+
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1); /* A. 21*/
+    show_bytes(valp, 2); /* B. 21 43*/
+    show_bytes(valp, 3); /* C. 21 43 65*/
+
+    return 0;
+
+}
--- section2/practice-problems/problem2-5.md
+++ section2/practice-problems/problem2-5.md
@@ -1,1 +1,30 @@
-## Practice Problem 2.5
+## Practice Problem 2.5 Hardback page 48
+
+
+- [main.c](./code/problem2dot5/main.c)
+
+- Consider the following three calls to show_bytes:
+
+```c
+int val = 0x87654321;
+byte_pointer valp = (byte_pointer) &val;
+show_bytes(valp, 1); /* A. */
+show_bytes(valp, 2); /* B. */
+show_bytes(valp, 3); /* C. */
+```
+
+- Indicate the values that will be printed by each call on a little-endian machine and on a big-endian machine:
+
+||Little endian|Big endian|
+|--|--|--|
+A.|21| 87|
+B.|21 43| 87 65|
+c.|21 43 65| 87 65 43|
+
+- Run the program on little-endian machine to check
+- gdb result viewing val using `x /4bx &val'
+- shows 4bytes in hexadecimal little-endian
+```bash
+(gdb) x /4bx &val
+0x7fffffffdec4:	0x21	0x43	0x65	0x87
+```
