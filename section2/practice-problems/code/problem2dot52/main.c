f4af4762a32bdee4e41764d9a345deeba951e1d6
mpflynnx <mpflynnx@gmail.com>
2025-06-19 10:00:31 +0000

    Added code for macro definitions to generate double-precision values for: +infinity, -infinity and -0

---
+++ section2/practice-problems/code/problem2dot52/main.c
@@ -0,0 +1,12 @@
+#define POS_INFINITY 1e400
+#define NEG_INFINITY (-POS_INFINITY)
+#define NEG_ZERO (-1.0/POS_INFINITY)
+
+int main() {
+
+    double p = POS_INFINITY;
+    double n = NEG_INFINITY;
+    double z = NEG_ZERO;
+
+    return 0;
+}
